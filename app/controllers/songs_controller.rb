982ca0b8b0e89b22fb2edbb621c8015377248346
github username <github email address>
2019-09-02 15:53:37 +0100

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,14 +12,13 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params)
-    # @song = Song.new(song_params)
+    @song = Song.new(song_params)
 
-    # if @song.save
-    #   redirect_to @song
-    # else
-    #   render :new
-    # end
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
   end
 
   def edit
